edec9ba409ba823f89fd0c1614f7824f92206026
Kovalov <kovalov.v@ajax.systems>
2021-05-10 16:24:10 +0300

    ch5

--- kirk/urls.py
+++ kirk/urls.py
@@ -3,5 +3,11 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # User management
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # Local apps
+    path('accounts/', include('users.urls')),
     path('', include('pages.urls')),
 ]
