36ac31c9eeab567e7fc24f39ad0d069f201b1e71
Kovalov <kovalov.v@ajax.systems>
2021-05-13 08:24:26 +0300

    ch6

--- kirk/settings.py
+++ kirk/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third-party
+    'crispy_forms',
+
+    # Local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
 ]
@@ -124,9 +128,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),]  #  static files inlocal development.
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')  # static files forproduction
+
+STATICFILES_FINDERS=[
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
-
 LOGOUT_REDIRECT_URL = 'home'
+
+# django-crispy-forms
+CRISPY_TEMPLATE_PACK='bootstrap4'
