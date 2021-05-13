8571430d3b44298612136b8653a3df82a280fd2d
Kovalov <kovalov.v@ajax.systems>
2021-05-13 10:43:27 +0300

    ch8

--- kirk/settings.py
+++ kirk/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!7=zp^3(wzp&_b%r^cbt(n3h2*t)fhh^4ld5jsjy7u%2bf99q^'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
